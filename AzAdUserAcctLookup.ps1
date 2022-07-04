409164a5f033f2b0b19676077a1016b6e5972f07
A-Ranch <86257516+A-Ranch@users.noreply.github.com>
2022-07-04 16:54:48 -0500

    Add files via upload

---
+++ AzAdUserAcctLookup.ps1
@@ -0,0 +1,15 @@
+﻿$UserList = Get-Content C:\Users\twinz\Desktop\Accounts.txt
+  
+ForEach ($Item in $UserList)
+{
+	$user = $null
+    $user = Get-AzADUser| Where-Object {$_.DisplayName -eq $Item}
+if ($user)
+	    {
+		$user | Out-File C:\Users\twinz\Desktop\existingAccounts.txt -encoding default -append
+	    }
+	else
+	    {
+			"$item does not exist" | Out-File C:\Users\twinz\Desktop\NotExistingAccount.txt -encoding default -append
+		}
+}
